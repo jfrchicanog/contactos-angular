72a4cc0b38099eb50eee13e6d52852ae589113bf
Francisco Chicano <chicano@lcc.uma.es>
2023-05-08 21:28:45 +0200

    swarm mode preparado

--- src/app/contactos.service.ts
+++ src/app/contactos.service.ts
@@ -2,28 +2,27 @@ import { Injectable } from '@angular/core';
 import {Contacto } from './contacto';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactosService {
-  private baseURI: string = 'http://localhost:8080/api/agenda/contactos';
-
   constructor(private http: HttpClient) { }
 
   getContactos(): Observable<Contacto []> {
-    return this.http.get<Contacto []>(this.baseURI);
+    return this.http.get<Contacto []>(environment.baseURI);
   }
 
   addContacto(contacto: Contacto): Observable<Contacto> {
-    return this.http.post<Contacto>(this.baseURI, contacto);
+    return this.http.post<Contacto>(environment.baseURI, contacto);
   }
 
   editarContacto(contacto: Contacto): Observable<Contacto> {
-    return this.http.put<Contacto>(this.baseURI + '/' + contacto.id, contacto);
+    return this.http.put<Contacto>(environment.baseURI + '/' + contacto.id, contacto);
   }
 
   eliminarcContacto(id: number): Observable<HttpResponse<string>> {
-    return this.http.delete(this.baseURI + '/' + id, {observe: "response", responseType: 'text'});
+    return this.http.delete(environment.baseURI + '/' + id, {observe: "response", responseType: 'text'});
   }
 }
